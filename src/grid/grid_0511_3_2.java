8f3904dfcc9585f7fa024c6a8e343b8628d41b73
santaoppa <ssanta1227@naver.com>
2022-05-12 06:17:30 +0900

    grid_0511_3_2

---
+++ src/grid/grid_0511_3_2.java
@@ -0,0 +1,37 @@
+package grid;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class grid_0511_3_2 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int k = sc.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+        int first = arr[n-1];
+        int second = arr[n-2];
+
+        int result = 0;
+        while (true){
+            for(int i = 0; i < k; i++){
+                if(m == 0) break;
+                result += first;
+                m-=1;
+            }
+            if(m == 0) break;
+            result += second;
+            m-=1;
+        }
+
+        System.out.println(result);
+    }
+}
---
+++ src/grid/grid_0511_3_2_2.java
@@ -0,0 +1,35 @@
+package grid;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class grid_0511_3_2_2 {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+
+        int n = sc.nextInt(); //5
+        int m = sc.nextInt(); //8
+        int k = sc.nextInt(); //3
+
+        int[] arr = new int[n]; // 2 4 5 4 6
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+        int first = arr[n-1]; //6
+        int second = arr[n-2]; //5
+
+        int count = (m/(k+1)) * k + (m%(k+1));
+
+        int result = 0;
+        result += (count)*first;
+        result += (m-count)*second;
+
+        System.out.println(result); //46
+
+
+
+
+    }
+}
