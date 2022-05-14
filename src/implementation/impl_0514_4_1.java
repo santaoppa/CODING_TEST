f2264b8cf2d0757e01c57c8089f9226cf220cfdb
santaoppa <ssanta1227@naver.com>
2022-05-14 21:41:06 +0900

    implementation_0514

---
+++ src/implementation/impl_0514_4_1.java
@@ -0,0 +1,35 @@
+package implementation;
+
+import java.util.Scanner;
+
+public class impl_0514_4_1 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        sc.nextLine(); // 버퍼 비우기
+        String[] plans = sc.nextLine().split(" ");
+        int x = 1, y = 1;
+
+        int[] dx = {0, 0, -1, 1};
+        int[] dy = {-1, 1, 0, 0};
+        char[] move_type = {'L', 'R', 'U', 'D'};
+
+        for (int i = 0; i < plans.length; i++) {
+            char plan = plans[i].charAt(0);
+            int nx = -1, ny = -1;
+            for(int j = 0; j < move_type.length; j++){
+                if (plan == move_type[j]){
+                    nx = x + dx[j];
+                    ny = y + dy[j];
+                }
+
+                if(nx < 1 || ny < 1 || nx > n || ny > n) continue;
+                x = nx;
+                y = ny;
+            }
+        }
+
+        System.out.println(x + " " + y);
+    }
+}
---
+++ src/implementation/impl_0514_4_2.java
@@ -0,0 +1,24 @@
+package implementation;
+
+import java.util.Scanner;
+
+public class impl_0514_4_2 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int count = 0;
+
+        for(int i = 0; i <= n; i++){
+            for(int j = 0; j < 60; j++){
+                for(int k = 0; k < 60; k++){
+                    if (i/10 == 3 || i%10 == 3 || j/10 == 3 || j%10 == 3 || k/10 == 3 || k%10 == 3) {
+                        count += 1;
+                    }
+                }
+            }
+        }
+
+        System.out.println(count);
+    }
+}
