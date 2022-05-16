463880e3113470e98e6d524720748992d5dc12ff
santaoppa <ssanta1227@naver.com>
2022-05-16 21:09:27 +0900

    implementation_0516

---
+++ src/implementation/impl_0516_4_3.java
@@ -0,0 +1,27 @@
+package implementation;
+
+import java.util.Scanner;
+
+public class impl_0516_4_3 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        String inputData = sc.nextLine();
+        int row = inputData.charAt(1) - '0';        //'0'의 아스키코드는 48
+        int col = inputData.charAt(0) - 'a' + 1;    //'a'의 아스키코드는 97
+
+        int[] dx = {-2, -1, 1, 2, 2, 1 ,-1, -2};
+        int[] dy = {-1, -2, -2, -1, 1 ,2 ,2, 1};
+
+        int result = 0;
+
+        for(int i = 0; i < 8; i++){
+           int nx = row + dx[i];
+           int ny = col + dy[i];
+
+           if(nx >= 1 && nx <= 8 && ny >= 1 && ny <= 8) result += 1;
+        }
+
+        System.out.println(result);
+    }
+}
