755f8bbce191923b1ee18d570cc2a711d61bd403
santaoppa <ssanta1227@naver.com>
2022-06-02 22:55:13 +0900

    search(dfs)

---
+++ src/search/search_0602_01.java
@@ -0,0 +1,49 @@
+package search;
+
+import java.util.Scanner;
+
+public class search_0602_01 {
+    public static int n, m;
+    public static int[][] graph = new int[1000][1000];
+
+    public static boolean dfs(int x, int y) {
+        if (x <= -1 || x >=n || y <= -1 || y >= m) {    //범위를 벗어나는 경우
+            return false;
+        }
+
+        if (graph[x][y] == 0) {
+            graph[x][y] = 1;
+            dfs(x - 1, y); //상
+            dfs(x, y - 1); //좌
+            dfs(x + 1, y); //하
+            dfs(x, y + 1); //우
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        m = sc.nextInt();
+        sc.nextLine();
+
+        for (int i = 0; i < n; i++) {
+            String str = sc.nextLine();
+            for (int j = 0; j < m; j++) {
+                graph[i][j] = str.charAt(j) - '0';
+            }
+        }
+
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (dfs(i, j)) {
+                    result += 1;
+                }
+            }
+        }
+        System.out.println(result);
+    }
+}
