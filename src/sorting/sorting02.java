ec977849bfb4614a7c70cd6186975c82c686bb7b
santaoppa <ssanta1227@naver.com>
2022-06-15 22:54:59 +0900

    commit

---
+++ src/sorting/sorting02.java
@@ -0,0 +1,23 @@
+package sorting;
+
+public class sorting02 {
+    public static void main(String args[]){
+        int[] array = {7, 5, 9, 0, 3 ,1, 6, 2, 4, 8};
+
+        for(int i=1; i<array.length; i++){
+            for(int j=i; j>0; j--){
+                if(array[j] < array[j-1]){
+                    int temp = array[j];
+                    array[j] = array[j-1];
+                    array[j-1] = temp;
+                }else{
+                    break;
+                }
+            }
+        }
+
+        for(int i=0; i<array.length; i++){
+            System.out.print(array[i] + " ");
+        }
+    }
+}
