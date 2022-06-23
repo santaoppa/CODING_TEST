d9a704ed08eddf0bb7c75370614e03c753471144
santaoppa <ssanta1227@naver.com>
2022-06-23 22:50:11 +0900

    commit

---
+++ src/sorting/B2751.java
@@ -0,0 +1,23 @@
+package sorting;
+
+import java.util.*;
+
+public class B2751{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        ArrayList<Integer> arr = new ArrayList<>();
+        for(int i =0; i<n; i++){
+            arr.add(sc.nextInt());
+        }
+
+        Collections.sort(arr);
+        StringBuilder sb = new StringBuilder();
+        for(int i =0; i<n; i++){
+            sb.append(arr.get(i) + "\n");
+        }
+
+        System.out.println(sb);
+    }
+}
