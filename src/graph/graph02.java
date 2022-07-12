37b83d290bd4ab10cf50bd38a24cce6867777efc
santaoppa <ssanta1227@naver.com>
2022-07-13 04:20:58 +0900

    commit

---
+++ src/graph/graph02.java
@@ -0,0 +1,63 @@
+package graph;
+
+import java.util.*;
+
+public class graph02 {
+    public static final int INF = (int) 1e9;
+    public static int n, m, start;
+    public static ArrayList<ArrayList<Node>> graph = new ArrayList<ArrayList<Node>>();
+    public static int[] d = new int[30001];
+
+    public static void dijkstra(int start) {
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.offer(new Node(start, 0));
+        d[start] = 0;
+        while(!pq.isEmpty()) {
+            Node node = pq.poll();
+            int dist = node.getDistance();
+            int now = node.getIndex();
+            if (d[now] < dist) continue;
+            for (int i = 0; i < graph.get(now).size(); i++) {
+                int cost = d[now] + graph.get(now).get(i).getDistance();
+                if (cost < d[graph.get(now).get(i).getIndex()]) {
+                    d[graph.get(now).get(i).getIndex()] = cost;
+                    pq.offer(new Node(graph.get(now).get(i).getIndex(), cost));
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        m = sc.nextInt();
+        start = sc.nextInt();
+
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<Node>());
+        }
+
+        for (int i = 0; i < m; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            int z = sc.nextInt();
+            graph.get(x).add(new Node(y, z));
+        }
+
+        Arrays.fill(d, INF);
+
+        dijkstra(start);
+
+        int count = 0;
+        int maxDistance = 0;
+        for (int i = 1; i <= n; i++) {
+            if (d[i] != INF) {
+                count += 1;
+                maxDistance = Math.max(maxDistance, d[i]);
+            }
+        }
+
+        System.out.println((count - 1) + " " + maxDistance);
+    }
+}
